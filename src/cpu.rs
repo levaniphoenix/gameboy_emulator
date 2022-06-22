78ddfdd2c2c88e83711700b5e0220301885c1ce3
Levani Bukhnikashvili <levanifenix.1234@gmail.com>
2022-06-22 22:16:44 +0400

    Implement all Add instructions

--- src/cpu.rs
+++ src/cpu.rs
@@ -23,12 +23,86 @@ impl CPU {
 
     pub fn call(&mut self, opcode: u8) -> u32 {
         match opcode {
+            0x09 => {
+                self.alu_add16(self.reg.bc());
+                2
+            }
+            0x19 => {
+                self.alu_add16(self.reg.de());
+                2
+            }
+            0x29 => {
+                self.alu_add16(self.reg.hl());
+                2
+            }
+            0x39 => {
+                self.alu_add16(self.reg.sp);
+                2
+            }
             0x80 => {
                 self.alu_add(self.reg.b, false);
                 1
             }
+            0x81 => {
+                self.alu_add(self.reg.c, false);
+                1
+            }
+            0x82 => {
+                self.alu_add(self.reg.d, false);
+                1
+            }
+            0x83 => {
+                self.alu_add(self.reg.e, false);
+                1
+            }
+            0x84 => {
+                self.alu_add(self.reg.h, false);
+                1
+            }
+            0x85 => {
+                self.alu_add(self.reg.l, false);
+                1
+            }
+            0x86 => {
+                let byte = self.mmu.read_byte(self.reg.hl());
+                self.alu_add(byte, false);
+                2
+            }
+            0x87 => {
+                self.alu_add(self.reg.a, false);
+                1
+            }
             0x88 => {
                 self.alu_add(self.reg.b, true);
+                1
+            }
+            0x89 => {
+                self.alu_add(self.reg.c, true);
+                1
+            }
+            0x8A => {
+                self.alu_add(self.reg.d, true);
+                1
+            }
+            0x8B => {
+                self.alu_add(self.reg.e, true);
+                1
+            }
+            0x8C => {
+                self.alu_add(self.reg.h, true);
+                1
+            }
+            0x8D => {
+                self.alu_add(self.reg.l, true);
+                1
+            }
+            0x8E => {
+                let byte = self.mmu.read_byte(self.reg.hl());
+                self.alu_add(byte, true);
+                2
+            }
+            0x8F => {
+                self.alu_add(self.reg.a, true);
                 1
             }
             0xC6 => {
@@ -40,6 +114,10 @@ impl CPU {
                 let n = self.fetchbyte();
                 self.alu_add(n, true);
                 2
+            }
+            0xE8 => {
+                self.reg.sp = self.alu_add16imm(self.reg.sp);
+                4
             }
             other => panic!("Instruction {:2X} is not implemented", other),
         }
@@ -62,6 +140,25 @@ impl CPU {
 
         self.reg.a = r;
     }
+    fn alu_add16(&mut self, b: u16) {
+        let a = self.reg.hl();
+        let r = a.wrapping_add(b);
+        self.reg
+            .flag(CpuFlag::H, (a & 0x07FF) + (b & 0x07FF) > 0x07FF);
+        self.reg.flag(CpuFlag::N, false);
+        self.reg.flag(CpuFlag::C, a > 0xFFFF - b);
+        self.reg.sethl(r);
+    }
+    fn alu_add16imm(&mut self, a: u16) -> u16 {
+        let b = self.fetchbyte() as u16;
+        self.reg.flag(CpuFlag::N, false);
+        self.reg.flag(CpuFlag::Z, false);
+        self.reg
+            .flag(CpuFlag::H, (a & 0x000F) + (b & 0x000F) > 0x000F);
+        self.reg
+            .flag(CpuFlag::C, (a & 0x00FF) + (b & 0x00FF) > 0x00FF);
+        return a.wrapping_add(b);
+    }
 }
 #[cfg(test)]
 mod test {
@@ -83,6 +180,22 @@ mod test {
         cpu.reg.a = 0x3C;
         let cycle = cpu.call(0xC6);
         assert_eq!(cpu.reg.a, 0x3B);
+        assert_eq!(cpu.reg.f, 0b00110000);
+    }
+
+    #[test]
+    fn bit16_add() {
+        let mut cpu = CPU::new();
+        cpu.reg.setbc(0x0605);
+        cpu.reg.sethl(0x8A23);
+        let cycle = cpu.call(0x09);
+        assert_eq!(cpu.reg.hl(), 0x9028);
+        assert_eq!(cpu.reg.f, 0b00100000);
+
+        cpu.reg.sp = 0xFFFF;
+        cpu.mmu.memory[0] = 2;
+        let cycle = cpu.call(0xE8);
+        assert_eq!(cpu.reg.sp, 0x0001);
         assert_eq!(cpu.reg.f, 0b00110000);
     }
 }
--- src/registers.rs
+++ src/registers.rs
@@ -45,4 +45,75 @@ impl Registers {
         let mask = flags as u8;
         self.f & mask > 0
     }
+    pub fn af(&self) -> u16 {
+        ((self.a as u16) << 8) | ((self.f & 0xF0) as u16)
+    }
+    pub fn bc(&self) -> u16 {
+        ((self.b as u16) << 8) | (self.c as u16)
+    }
+    pub fn de(&self) -> u16 {
+        ((self.d as u16) << 8) | (self.e as u16)
+    }
+    pub fn hl(&self) -> u16 {
+        ((self.h as u16) << 8) | (self.l as u16)
+    }
+    pub fn setaf(&mut self, value: u16) {
+        self.a = (value >> 8) as u8;
+        self.f = (value & 0x00F0) as u8;
+    }
+    pub fn setbc(&mut self, value: u16) {
+        self.b = (value >> 8) as u8;
+        self.c = (value & 0x00FF) as u8;
+    }
+    pub fn setde(&mut self, value: u16) {
+        self.d = (value >> 8) as u8;
+        self.e = (value & 0x00FF) as u8;
+    }
+    pub fn sethl(&mut self, value: u16) {
+        self.h = (value >> 8) as u8;
+        self.l = (value & 0x00FF) as u8;
+    }
+    pub fn hld(&mut self) -> u16 {
+        let res = self.hl();
+        self.sethl(res - 1);
+        res
+    }
+    pub fn hli(&mut self) -> u16 {
+        let res = self.hl();
+        self.sethl(res + 1);
+        res
+    }
+    #[cfg(test)]
+    fn setf(&mut self, flags: u8) {
+        self.f = flags & 0xF0;
+    }
+}
+#[cfg(test)]
+mod test {
+    use super::Registers;
+    #[test]
+    fn wide_registers() {
+        let mut reg = Registers::new();
+        reg.a = 0x12;
+        reg.setf(0x23);
+        reg.b = 0x34;
+        reg.c = 0x45;
+        reg.d = 0x56;
+        reg.e = 0x67;
+        reg.h = 0x78;
+        reg.l = 0x89;
+        assert_eq!(reg.af(), 0x1220);
+        assert_eq!(reg.bc(), 0x3445);
+        assert_eq!(reg.de(), 0x5667);
+        assert_eq!(reg.hl(), 0x7889);
+
+        reg.setaf(0x1111);
+        reg.setbc(0x1111);
+        reg.setde(0x1111);
+        reg.sethl(0x1111);
+        assert_eq!(reg.af(), 0x1110);
+        assert_eq!(reg.bc(), 0x1111);
+        assert_eq!(reg.de(), 0x1111);
+        assert_eq!(reg.hl(), 0x1111);
+    }
 }
